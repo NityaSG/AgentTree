989557b05a5f33e86b0c46376139a465a675ea65
Nitya Singh <76156577+NityaSG@users.noreply.github.com>
2024-02-22 00:30:58 +0530

    change

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 class CalculatorBase:
     def __init__(self, output_number):
-        self.input_number = None  # Will be set from the output of another node
+        self.input_number = None
         self.output_number = output_number
 
     def calculate(self):
@@ -18,7 +18,7 @@ class Node:
     def __init__(self, calculator, name):
         self.calculator = calculator
         self.name = name
-        self.inputs = []  # List of Node names whose output is input to this Node
+        self.inputs = []
         self.output = None
 
     def add_input(self, node_name):
@@ -27,55 +27,48 @@ class Node:
 class Graph:
     def __init__(self):
         self.nodes = {}
-        self.execution_order = []
+        self.initial_nodes = []
+        self.final_nodes = []
 
-    def add_node(self, node):
+    def add_node(self, node, is_initial=False, is_final=False):
         self.nodes[node.name] = node
+        if is_initial:
+            self.initial_nodes.append(node.name)
+        if is_final:
+            self.final_nodes.append(node.name)
 
     def add_edge(self, from_node_name, to_node_name):
         self.nodes[to_node_name].add_input(from_node_name)
 
-    def find_execution_order(self):
-        visited = set()
-        self.execution_order = []
-
-        def visit(node_name):
-            if node_name not in visited:
-                visited.add(node_name)
-                for next_node_name in self.nodes[node_name].inputs:
-                    visit(next_node_name)
-                self.execution_order.append(node_name)
-
-        for node_name in self.nodes:
-            visit(node_name)
+    def set_initial_input(self, node_name, input_value):
+        if node_name in self.nodes:
+            self.nodes[node_name].calculator.input_number = input_value
 
     def execute(self):
-        self.find_execution_order()
-        for node_name in self.execution_order:
+        for node_name in self.nodes:
             node = self.nodes[node_name]
             if node.inputs:
-                # For simplicity, assuming a single input
-                input_node_name = node.inputs[0]
-                input_node = self.nodes[input_node_name]
-                node.calculator.input_number = input_node.output
+                for input_node_name in node.inputs:
+                    input_node = self.nodes[input_node_name]
+                    node.calculator.input_number = input_node.output
             node.output = node.calculator.calculate()
 
 # Example usage
 graph = Graph()
 
-# Creating nodes with operations
-graph.add_node(Node(AddCalculator(5), 'add1'))
-graph.add_node(Node(MultiplyCalculator(2), 'mul1'))
+# Creating nodes with operations and marking initial and final states
+graph.add_node(Node(AddCalculator(5), 'add1'), is_initial=True)
+graph.add_node(Node(MultiplyCalculator(2), 'mul1'), is_final=True)
 
 # Defining connections (edges)
 graph.add_edge('add1', 'mul1')
 
-# Setting an initial input value
-graph.nodes['add1'].calculator.input_number = 10
+# Setting the main input for the initial node
+graph.set_initial_input('add1', 10)
 
 # Execute the graph
 graph.execute()
 
-# Accessing results
-print(f"Output of add1: {graph.nodes['add1'].output}")  # Should be 15
-print(f"Output of mul1: {graph.nodes['mul1'].output}")  # Should be 30
+# Accessing results from final nodes
+for final_node_name in graph.final_nodes:
+    print(f"Output of {final_node_name}: {graph.nodes[final_node_name].output}")
